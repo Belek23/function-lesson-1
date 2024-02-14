c7b8ba60387bf07d8dce08989a6b3411538b8793
Belek23 <beshik2013@yandex.ru>
2024-02-14 18:17:33 +0300

    add lesson:task2

--- task2/Program.cs
+++ task2/Program.cs
@@ -1,2 +1,17 @@
 ﻿// Задача №2
-// Вывод на экран квадратов числе от 1 до N
+// Вывод на экран квадратов числа 5, 10, 15.
+
+void PrintSquares (int limit)
+{
+    int i = 1;
+    while(i <= limit)
+    {
+        System.Console.Write($"{i*i} ");
+        i++;
+    }
+}
+
+PrintSquares(5);
+PrintSquares(10);
+PrintSquares(15);
+
